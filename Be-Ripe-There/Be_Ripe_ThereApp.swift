5b43f867e914f9af32734f28834e2de0df9afb7c
Kevin Huynh <kvhuynh820@gmail.com>
2024-02-12 13:40:00 -0800

    created repository

---
+++ Be-Ripe-There/Be_Ripe_ThereApp.swift
@@ -0,0 +1,17 @@
+//
+//  Be_Ripe_ThereApp.swift
+//  Be-Ripe-There
+//
+//  Created by Kevin Huynh on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Be_Ripe_ThereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Be-Ripe-ThereUITests/Be_Ripe_ThereUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Be_Ripe_ThereUITestsLaunchTests.swift
+//  Be-Ripe-ThereUITests
+//
+//  Created by Kevin Huynh on 2/12/24.
+//
+
+import XCTest
+
+final class Be_Ripe_ThereUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
